8ed99fbf6c855a9ebb4bb65c1ee0235552261091
EvanEbert <evan.ebert17@gmail.com>
2021-07-22 08:17:18 -0500

    added synopsys (literally just placeholder text imo) and more css styling.

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+var userText = document.getElementById("textcontent");
+var inventoryButton = document.getElementById("inventorybutton");
+var mapButton = document.getElementById("mapbutton");
+var mainText = document.getElementById("maincontent");
+var mainTextDiv = document.querySelector(".maintext");
+var mainmenuButton = document.getElementById("start")
+
