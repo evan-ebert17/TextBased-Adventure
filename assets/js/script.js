2cc96e8b3ca925477c2c9a3627fa9572eb7492d8
EvanEbert <evan.ebert17@gmail.com>
2021-07-23 11:23:51 -0500

    added whenever you push the start button shows the form elements at the top

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,7 @@
 var menuDiv = document.getElementById("menuscreen");
 var mainmenuButton = document.getElementById("start");
 
+var formEl = document.getElementById('form');
 var mainTextContentDiv = document.querySelector(".maintext");
 var mainTextDiv = document.getElementById("mainholder");
 var mainText = document.getElementById("maincontent");
@@ -21,6 +22,16 @@ var mapDiv = document.getElementById("mapdiv");
 var mapContent = document.getElementById("map");
 var mapButton = document.getElementById('mapbutton');
 
+var pIndex = 0;
+
+var housePrompts = [
+    {
+        init: "you wake up in your bed, head slightly aching from last nights saunter through a few local places. as it stands, your name is Daniel and you are a detective. currently, you are on the case of the Michael Derrick gang, a notorius crime syndicate that has been pedaling drugs and alchohol. it is currently bright outside on a sunday afternoon.",
+        prompts: ["next room", "next room2", "next room3", "nextroom4"],
+
+    },
+]
+
 let title;
 let lastPara;
 
@@ -31,24 +42,33 @@ function createTitle() {
     titleffect.append(title);
 }
 
+function runTopForm() {
+    if (formEl.style.display === "none") {
+        formEl.style.display = "block";
+    } else {
+        formEl.style.display = "none";
+    }
+}
+
 //this is me trying to stop the submitting text from refreshing the page.
 //submitText.addEventListener('submit', function (event) {
-    //    event.preventDefault();
-    //});
-    
+//    event.preventDefault();
+//});
+
 mainmenuButton.addEventListener('click', function (event) {
     event.preventDefault();
     menuDiv.innerHTML = '';
     createTitle();
+    runTopForm();
     mainText.textContent = "you wake up in your bed, head slightly aching from last nights saunter through a few local places. as it stands, your name is Daniel and you are a detective. currently, you are on the case of the Michael Derrick gang, a notorius crime syndicate that has been pedaling drugs and alchohol. it is currently bright outside on a sunday afternoon."
 })
 
 function hideInv() {
-   if(inventorycontent.style.display === "none") {
-       inventorycontent.style.display = "block";
-   } else {
-       inventorycontent.style.display = "none";
-   }
+    if (inventorycontent.style.display === "none") {
+        inventorycontent.style.display = "block";
+    } else {
+        inventorycontent.style.display = "none";
+    }
 }
 
 inventoryButton.addEventListener('click', function (event) {
@@ -59,13 +79,23 @@ inventoryButton.addEventListener('click', function (event) {
 })
 
 function hideMap() {
-    if(mapContent.style.display === "none") {
+    if (mapContent.style.display === "none") {
         mapContent.style.display = "block";
     } else {
         mapContent.style.display = "none";
     }
- }
- 
+}
+
+function promptCycling() {
+    //when I input text and enter, it takes the input and goes to a positon in the array based on the input
+    //additionally, once I am at the new point in the array I can go back and that the position from the array I have choices that I would not have otherwise
+    //
+    mainText.textContent = "";
+    currentPrompt = housePrompts[pIndex];
+    questionSection.innerHTML = currentPrompt.question;
+
+}
+
 
 mapButton.addEventListener('click', function (event) {
     event.preventDefault();
