35c96bf47de5d283893136e25cdc6efbed8863a6
EvanEbert <evan.ebert17@gmail.com>
2021-07-23 16:19:55 -0500

    fixed submit refreshing the page and added indexCubeChar

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,13 +22,67 @@ var mapDiv = document.getElementById("mapdiv");
 var mapContent = document.getElementById("map");
 var mapButton = document.getElementById('mapbutton');
 
-var pIndex = 0;
+//accessing prompts/rooms
+//give every prompt in the game a value (1-737);
+//when player enters a command they will go to a value and it will pull up the corresponding text box
+//to save the game, it just stores this value in localstorage and then whenever player loads, it goes to the last saved value-prompt
+let pIndex = 0;
+const up = + 2;
+const down = - 2;
+const left = + 1;
+const right = - 1;
 
+var allPrompts = [];
 var housePrompts = [
     {
         init: "you wake up in your bed, head slightly aching from last nights saunter through a few local places. as it stands, your name is Daniel and you are a detective. currently, you are on the case of the Michael Derrick gang, a notorius crime syndicate that has been pedaling drugs and alchohol. it is currently bright outside on a sunday afternoon.",
-        prompts: ["next room", "next room2", "next room3", "nextroom4"],
-
+        roomelVal: 0,
+        ifrepeated: 0,
+    },
+    {
+        room: 'this is your bathroom',
+        roomelVal: -2,
+    },
+    {
+        room: 'this is bedroom-storage hallway',
+        roomelVal: 1,
+    },
+    {
+        room: 'this is your front attic',
+        roomelVal: 4,
+    },
+    {
+        room: 'this is your back attic',
+        roomelVal: 5,
+    },
+    {
+        room: "storage",
+        roomelVal: 2,
+    },
+    {
+        room: "office",
+        roomelVal: 0,
+        ifrepeated: 5,
+    },
+    {
+        room: "hall office",
+        roomelVal: -1,
+    },
+    {
+        room: "hall living",
+        roomelVal: -3,
+    },
+    {
+        room: "livingroom",
+        roomelVal: -4,
+    },
+    {
+        room: 'this is your kitchen',
+        roomelVal: -5,
+    },
+    {
+        room: "front door",
+        roomelVal: -6,
     },
 ]
 
@@ -50,10 +104,12 @@ function runTopForm() {
     }
 }
 
-//this is me trying to stop the submitting text from refreshing the page.
-submitText.addEventListener('submit', function (event) {
-   event.preventDefault();
-});
+formEl.addEventListener('submit', function (event) {
+    if (event) {
+        userText.value = '';
+        event.preventDefault();
+    }
+})
 
 mainmenuButton.addEventListener('click', function (event) {
     event.preventDefault();
@@ -86,13 +142,18 @@ function hideMap() {
     }
 }
 
+let currentPrompt;
+
 function promptCycling() {
     //when I input text and enter, it takes the input and goes to a positon in the array based on the input
     //additionally, once I am at the new point in the array I can go back and that the position from the array I have choices that I would not have otherwise
-    //
-    mainText.textContent = "";
     currentPrompt = housePrompts[pIndex];
-    questionSection.innerHTML = currentPrompt.question;
+    console.log(currentPrompt[pIndex]);
+    mainText.textContent = currentPrompt.room;
+    for (let i = 0; i < currentPrompt.length; i++) {
+        const element = array[i];
+
+    }
 
 }
 
@@ -102,6 +163,7 @@ mapButton.addEventListener('click', function (event) {
     mapContent.classList.add('.map')
     mapContent.appendChild(houseMap);
     hideMap();
+    indexCubeChar();
 });
 
 function altCommands() {
@@ -112,6 +174,17 @@ function altCommands() {
         // commandlist.classList.add(".smallertext");
         // commandlist.textContent = "Commands:"
         // mainholder.append(commandlist);
-        mainText.textContent = "help: displays this page, look: inspects a thing you specify, north/south/west/east: moves you in said direction, talk: talk to a person..."
+        mainText.textContent = "help: displays this page, look: inspects a thing you specify, north or up/south or down/west or left/east or right: moves you in said direction, talk: talk to a person..."
+    }
+}
+
+function indexCubeChar() {
+    const indexCubeChar = document.createElement('button');
+    if (mapContent.style.display === "block") {
+        indexCubeChar.style.backgroundColor = '#90ee90';
+        indexCubeChar.style.zIndex = 1;
+        mapContent.append(indexCubeChar)
+    } else {
+        return;
     }
 }
