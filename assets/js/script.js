8e2bd53f5b6b32bc20391395b7d32860ebdcd745
EvanEbert <evan.ebert17@gmail.com>
2021-07-26 13:14:20 -0500

    organized functions and added promptCycling() functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -35,7 +35,7 @@ const right = - 1;
 var allPrompts = [];
 var housePrompts = [
     {
-        init: "you wake up in your bed, head slightly aching from last nights saunter through a few local places. as it stands, your name is Daniel and you are a detective. currently, you are on the case of the Michael Derrick gang, a notorius crime syndicate that has been pedaling drugs and alchohol. it is currently bright outside on a sunday afternoon.",
+        room: "you wake up in your bed, head slightly aching from last nights saunter through a few local places. as it stands, your name is Daniel and you are a detective. currently, you are on the case of the Michael Derrick gang, a notorius crime syndicate that has been pedaling drugs and alchohol. it is currently bright outside on a sunday afternoon.",
         roomelVal: 0,
         ifrepeated: 0,
     },
@@ -86,16 +86,29 @@ var housePrompts = [
     },
 ]
 
-let title;
+let currentPrompt = housePrompts[pIndex];
 let lastPara;
-
+let title;
 function createTitle() {
     title = document.createElement('h1');
     title.innerHTML = "Gost"
     var titleffect = document.getElementById('titleffect');
     titleffect.append(title);
 }
-
+formEl.addEventListener('submit', function (event) {
+    if (event) {
+        userText.value = '';
+        event.preventDefault();
+    }
+    promptCycling();
+});
+mainmenuButton.addEventListener('click', function (event) {
+    event.preventDefault();
+    menuDiv.innerHTML = '';
+    createTitle();
+    runTopForm();
+    mainText.textContent = currentPrompt.room;
+})
 function runTopForm() {
     if (formEl.style.display === "none") {
         formEl.style.display = "block";
@@ -103,37 +116,6 @@ function runTopForm() {
         formEl.style.display = "none";
     }
 }
-
-formEl.addEventListener('submit', function (event) {
-    if (event) {
-        userText.value = '';
-        event.preventDefault();
-    }
-})
-
-mainmenuButton.addEventListener('click', function (event) {
-    event.preventDefault();
-    menuDiv.innerHTML = '';
-    createTitle();
-    runTopForm();
-    mainText.textContent = "you wake up in your bed, head slightly aching from last nights saunter through a few local places. as it stands, your name is Daniel and you are a detective. currently, you are on the case of the Michael Derrick gang, a notorius crime syndicate that has been pedaling drugs and alchohol. it is currently bright outside on a sunday afternoon."
-})
-
-function hideInv() {
-    if (inventorycontent.style.display === "none") {
-        inventorycontent.style.display = "block";
-    } else {
-        inventorycontent.style.display = "none";
-    }
-}
-
-inventoryButton.addEventListener('click', function (event) {
-    inventorycontent.textContent = '';
-    event.preventDefault();
-    inventorycontent.textContent = "bomb, rope, gun.";
-    hideInv();
-})
-
 function hideMap() {
     if (mapContent.style.display === "none") {
         mapContent.style.display = "block";
@@ -141,23 +123,19 @@ function hideMap() {
         mapContent.style.display = "none";
     }
 }
-
-let currentPrompt;
-
-function promptCycling() {
-    //when I input text and enter, it takes the input and goes to a positon in the array based on the input
-    //additionally, once I am at the new point in the array I can go back and that the position from the array I have choices that I would not have otherwise
-    currentPrompt = housePrompts[pIndex];
-    console.log(currentPrompt[pIndex]);
-    mainText.textContent = currentPrompt.room;
-    for (let i = 0; i < currentPrompt.length; i++) {
-        const element = array[i];
-
+inventoryButton.addEventListener('click', function (event) {
+    inventorycontent.textContent = '';
+    event.preventDefault();
+    inventorycontent.textContent = "bomb, rope, gun.";
+    hideInv();
+});
+function hideInv() {
+    if (inventorycontent.style.display === "none") {
+        inventorycontent.style.display = "block";
+    } else {
+        inventorycontent.style.display = "none";
     }
-
 }
-
-
 mapButton.addEventListener('click', function (event) {
     event.preventDefault();
     mapContent.classList.add('.map')
@@ -165,7 +143,30 @@ mapButton.addEventListener('click', function (event) {
     hideMap();
     indexCubeChar();
 });
+function indexCubeChar() {
+    const indexCubeChar = document.createElement('button');
+    if (mapContent.style.display === "block") {
+        indexCubeChar.style.backgroundColor = '#90ee90';
+        indexCubeChar.style.zIndex = 1;
+        mapContent.append(indexCubeChar)
+    } else {
+        return;
+    }
+};
 
+function promptCycling() {
+    //when I input text and enter, it takes the input and goes to a positon in the array based on the input
+    //additionally, once I am at the new point in the array I can go back and that the position from the array I have choices that I would not have otherwise
+    currentPrompt = housePrompts[pIndex];
+    console.log(currentPrompt);
+    mainText.textContent = currentPrompt.room;
+    for (let i = 0; i < currentPrompt.length; i++) {
+        const houseArray = currentPrompt
+        console.log(houseArray);
+
+    }
+
+};
 function altCommands() {
     if (userText === "help" || userText === "commands") {
         mainText.textContent = '';
@@ -175,16 +176,5 @@ function altCommands() {
         // commandlist.textContent = "Commands:"
         // mainholder.append(commandlist);
         mainText.textContent = "help: displays this page, look: inspects a thing you specify, north or up/south or down/west or left/east or right: moves you in said direction, talk: talk to a person..."
-    }
-}
-
-function indexCubeChar() {
-    const indexCubeChar = document.createElement('button');
-    if (mapContent.style.display === "block") {
-        indexCubeChar.style.backgroundColor = '#90ee90';
-        indexCubeChar.style.zIndex = 1;
-        mapContent.append(indexCubeChar)
-    } else {
-        return;
     }
 }
